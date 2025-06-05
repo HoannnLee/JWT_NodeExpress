434768215cff8ac0af35de2716c582f53d3e3ff7
HoannnLee <Leecghoan@gmail.com>
2025-06-05 15:32:57 +0700

    handlelogin complete, bcrypt.compare password

--- src/services/userService.js
+++ src/services/userService.js
@@ -23,7 +23,44 @@ const createUserService = async (email, name, password) => {
 }
 
 
+//  1 .lấy ra user theo email và password
+//  2. kiểm tra xem có tồn tại user hay không
+//  3. nếu có thì bắt đầu so sánh password
+//  4. neu so sánh đúng thì tạo access token
+//  5. nếu sai thì trả về lỗi
+//  2.1 trường hợp else trả về lỗi
+
+const loginService = async (inputEmail, password) => {
+    try {
+
+        const user = await User.findOne({email : inputEmail});
+        if (user){
+            const isMatchPasword = await bcrypt.compare(password, user.password);
+            if(!isMatchPasword){
+                return {
+                    EC:2,
+                    EM:"Email/Mật khẩu không hợp"
+                }
+            }else{
+                return "Create Access Token Success";
+            }
+        }
+        else{
+            return{
+                EC:1,
+                EM:" Người dung không tồn tại"
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+
 
 module.exports = {
-    createUserService
+    createUserService,
+    loginService
 }
